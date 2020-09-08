58f0180a9402d0ed356be844b72e2d4f1290c5bf
mvwtest <mvwtest@163.com>
2020-09-08 06:28:22 -0700

    Update cc1101_Transmitt_Serial_minimal.ino

--- examples/CC1101 default examples/cc1101_Transmitt_Serial_minimal/cc1101_Transmitt_Serial_minimal.ino
+++ examples/CC1101 default examples/cc1101_Transmitt_Serial_minimal/cc1101_Transmitt_Serial_minimal.ino
@@ -2,22 +2,15 @@
 //https://github.com/LSatan/SmartRC-CC1101-Driver-Lib
 // mod by Little_S@tan
 
-#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <ELECHOUSE_CC1101_SRC_DRV_stm32.h>
 
-int gdo0;
+int gdo0=28;
 
 const int n = 61;
 byte buffer[n] = "";
 
 void setup() {
-
-#ifdef ESP32
-gdo0 = 2;  // for esp32! GDO0 on GPIO pin 2.
-#elif ESP8266
-gdo0 = 5;  // for esp8266! GDO0 on pin 5 = D1.
-#else
-gdo0 = 6;  // for Arduino! GDO0 on pin 6.
-#endif 
+ 
   
     Serial.begin(9600);
     ELECHOUSE_cc1101.Init();               // must be set to initialize the cc1101!
